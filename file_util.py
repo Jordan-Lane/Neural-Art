fc0ac151b1505107c9d894cc76b21c766752a5cc
Jordan Lane <jlane@ualberta.ca>
2021-01-12 01:27:17 -0800

    Numpy network v1

---
+++ file_util.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy
+import os
+
+from pathlib import Path
+
+def make_dir(dir_name):
+    output_directory_fullpath = os.path.join(str(Path.cwd()), dir_name)
+    Path(output_directory_fullpath).mkdir(parents=True, exist_ok=True)
+
+    return output_directory_fullpath
+
+def save_numpy_image(data, filename):
+    output_directory = "image_examples"
+    output_directory_fullpath = make_dir(output_directory)
+
+    print("\nSaving image to " + output_directory + ": " + filename)
+    file_full_path = os.path.join(output_directory_fullpath, filename)
+
+    cv2.imwrite(file_full_path, data, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
---
+++ numpy_activation.py
@@ -0,0 +1,18 @@
+import numpy
+
+def tanh(matrix):
+    return numpy.tanh(matrix)
+
+def sigmoid(matrix):
+    return 1/(1 + numpy.exp(-matrix))
+
+def relu(matrix):
+    return numpy.maximum(matrix, 0, matrix)
+
+def softmax(matrix):
+    expo = numpy.exp(matrix)
+    expo_sum = numpy.sum(numpy.exp(matrix))
+    return expo/expo_sum
+
+# Meta programming idea: Use https://www.tutorialspoint.com/How-to-list-all-functions-in-a-Python-module
+#   to create a dict of names -> functions for use in the numpy_image activation layer
