3dd73c267b42ee7f5cc3b8fa684b522ccb2d2f67
Jordan Lane <jlane@ualberta.ca>
2021-04-07 14:22:41 -0700

    Move main program and arg parsing to new file main.py

--- src/art_net.py
+++ src/art_net.py
@@ -6,7 +6,6 @@ import random
 
 from file_util import save_numpy_image
 from inspect import getmembers, isfunction
-#from numpy_activation import *
 
 class NumpyArtGenerator:
     """ Generates imagery using randomness and a fully connected forward propagation network """
@@ -81,8 +80,6 @@ class NumpyArtGenerator:
             activation_func = self.activation[1]
             results = activation_func(numpy.matmul(results, W))
 
-        # TODO: Consider actual normalization (pros: more flexible with activation function... cons: probabaly a lot slower)
-        results = (1 + results)/2.0
         results = (255.0 * results.reshape(nrows, ncols, results.shape[-1])).astype(numpy.uint8)
         return results
 
@@ -91,52 +88,6 @@ class NumpyArtGenerator:
         inputs = self.__generate_input()
         return self.forward_prop(inputs)
 
-
-def single_img_generation(args):
-    if args.seed is None:
-        args.seed = random.randint(seed_min, seed_max)
-    if args.layers is None:
-        args.layers = random.randint(layers_min, layers_max)
-    if args.width is None:
-        args.width = random.randint(width_min, width_max)
-
-    if args.resolution is None:
-        args.resolution = default_resolution
-    else:
-        args.resolution = tuple(args.resolution)
-
-    generator = NumpyArtGenerator(args.resolution, args.seed, args.layers, args.width, args.activation)
-    numpy_image = generator.run()
-
-    filename = str(generator) + ".jpg"
-    output_directory = "../images"
-    save_numpy_image(numpy_image, filename, output_directory)
-
-
-if __name__ == "__main__":
-    seed_min = 0
-    seed_max = 2147483647
-    layers_min = 0
-    layers_max = 50
-    width_min = 0
-    width_max = 20
-    default_resolution = (1920, 1080)
-
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
-
-    single_parser = subparsers.add_parser('single')
-    single_parser.add_argument("-s", "--seed", type=int, help="Seed value used by numpy random. Default is a random value between " + str(seed_min) + " - " + str(seed_max))
-    single_parser.add_argument("-l", "--layers", type=int, help="Number of hidden layers. Default is a random int between " + str(layers_min) + " - " + str(layers_max))
-    single_parser.add_argument("-w", "--width", type=int, help="Number of perceptrons in each hidden layer. Default is a random int between " + str(width_min) + " - " + str(width_max))
-    single_parser.add_argument("-r", '--resolution', nargs=2, type=int, help="Resolution of output image. Default is " + str(default_resolution))
-    single_parser.add_argument("-a", "--activation", default="tanh", help="Activation function name used in every hidden layer. Activation functions can be found in the numpy_activation file.")
-    single_parser.set_defaults(func=single_img_generation)
-    
-    args = parser.parse_args()
-
-    # TODO: Check that a func exists and display useful dialog to user
-    args.func(args)
 
 
 
